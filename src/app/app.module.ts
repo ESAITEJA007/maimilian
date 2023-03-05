cd873e47c7fb62c82ff1c579a2b28b39d2d62a0d
saiteja <enimidigandlasaiteja@gmail.com>
2023-03-05 16:47:16 -0600

    Added header main-body using separate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainBodyComponent } from './main-body/main-body.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainBodyComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
